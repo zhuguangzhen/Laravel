0a2d1f1aef5d0b1a13c8855974fbceb8dac32123
zhugz <1164401504@qq.com>
2018-12-21 17:39:40 +0800

    注册页

---
+++ resources/views/business/login/index.blade.php
@@ -0,0 +1,56 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312">
+<title>jQuery/CSS3分步骤注册表单切换动画在线演示</title>
+<link rel="stylesheet" media="screen" href="{{ asset('/css/login/css.css') }}" />
+<link rel="stylesheet" href="{{ asset('/css/business/templatemo-style.css') }}">
+<link rel="icon" href="/image/webimg/1.png">
+</head>
+
+<form id="msform">
+	<!-- progressbar -->
+	<ul id="progressbar">
+		<li class="active">Account Setup</li>
+		<li>Social Profiles</li>
+		<li>Personal Details</li>
+	</ul>
+	<!-- fieldsets -->
+	<fieldset>
+		<h2 class="fs-title">Create your account</h2>
+		<h3 class="fs-subtitle">This is step 1</h3>
+		<input type="text" name="email" placeholder="Email" />
+		<input type="password" name="pass" placeholder="Password" />
+		<input type="password" name="cpass" placeholder="Confirm Password" />
+		<input type="button" name="next" class="next action-button" value="Next" />
+	</fieldset>
+	<fieldset>
+		<h2 class="fs-title">Social Profiles</h2>
+		<h3 class="fs-subtitle">Your presence on the social network</h3>
+		<input type="text" name="twitter" placeholder="Twitter" />
+		<input type="text" name="facebook" placeholder="Facebook" />
+		<input type="text" name="gplus" placeholder="Google Plus" />
+		<input type="button" name="previous" class="previous action-button" value="Previous" />
+		<input type="button" name="next" class="next action-button" value="Next" />
+	</fieldset>
+	<fieldset>
+		<h2 class="fs-title">Personal Details</h2>
+		<h3 class="fs-subtitle">We will never sell it</h3>
+		<input type="text" name="fname" placeholder="First Name" />
+		<input type="text" name="lname" placeholder="Last Name" />
+		<input type="text" name="phone" placeholder="Phone" />
+		<textarea name="address" placeholder="Address"></textarea>
+		<input type="button" name="previous" class="previous action-button" value="Previous" />
+		<input type="submit" name="submit" class="submit action-button" value="Submit" />
+	</fieldset>
+</form>
+<script src="{{ asset('/js/login/jquery-1.9.1.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('/js/login/jquery.easing.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('/js/login/jQuery.time.js') }}" type="text/javascript"></script>
+<br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br>
+        <div class="footer-link">
+          <p>Copyright © 2018 for Flying-monkey Design </p><p><a rel="nofollow" href="#" target="_parent">Welcome to visit and exchange</a>
+                    More please login <a href="" target="_blank" title="">Flying </a>- <a href="" title="" target="_blank">monkey</a></p>
+        </div>
+</body>
+</html>
