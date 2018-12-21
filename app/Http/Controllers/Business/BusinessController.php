1f2e91f50cd4480ca5facf8bc738ca3c21ce5567
zhugz <1164401504@qq.com>
2018-12-21 10:29:37 +0800

    数据库连接测试

--- app/Http/Controllers/Business/BusinessController.php
+++ app/Http/Controllers/Business/BusinessController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Business;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use DB;
 
 class BusinessController extends Controller
 {
     public function Index(){
-    
+    	$a = DB::table('z_admin')->get();
+    	dd($a);
     	return view('/business/index/index');
     }
 }
--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '106.12.219.30'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'php'),
+            'username' => env('DB_USERNAME', 'joker'),
+            'password' => env('DB_PASSWORD', '!br13jhhrh6'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
