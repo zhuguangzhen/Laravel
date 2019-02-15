68dc8c46e1b8d7ee96b3ddf928c2c3c7ae198235
zhugz <1164401504@qq.com>
2019-02-15 18:10:44 +0800

    123

--- app/Http/Controllers/News/IndexController.php
+++ app/Http/Controllers/News/IndexController.php
@@ -25,8 +25,18 @@ class IndexController extends Controller
 
       	$data['newslist'] =  DB::table('n_news')->where('isuse','1')->get();
 
-      	$data['newstitle'] =  DB::table('n_article')->select('id','title','create_at','nid','ismain','image')->get();
+      	$newstitle =  DB::table('n_article')->select('id','title','create_at','nid','ismain','image')->get();
+        foreach($newstitle as $v){
+          $v->create = date('Y-m-d',strtotime($v->create_at));
+            if($v->ismain){
+                $data['ismin'][] = $v;
+            }else{
+                $data['notmain'][$v->nid][] = $v;
+            }
+        }
 
+        $data['product'] = DB::table('n_product_desc')->where('isuse','1')->get();
+// dd($data);
         return view('/news/index/index',$data);
     }
 
